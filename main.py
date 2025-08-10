6f1f50570fa5296e1008278fef1acbbea95ced8b
Alex Kussauer <akussauer@gmail.com>
2025-08-10 11:04:58 +0100

    added UV project manager

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+This main.py script doesnt actually do much, it is just a placeholder for the UV project management
+"""
+
+def main():
+    print("Hello from portfolio!")
+
+
+if __name__ == "__main__":
+    main()
