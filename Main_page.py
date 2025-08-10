252f79f702677a9d74a4d07b53b5e1e8e78b4b21
Alex Kussauer <akussauer@gmail.com>
2025-08-10 10:19:05 +0100

    test

--- Main_page.py
+++ Main_page.py
@@ -9,7 +9,7 @@ st.set_page_config(
 
 ## Main Page title
 st.image("StreamLit Work Portfolio/assets/headshot.JPEG", width=200)
-st.title("Welcome to My Portfolio")
+st.title("welcome to My Portfolio")
 # --- Projects ---
 st.markdown("### About Me")
 st.write("""
