e29ba7a6f609120f1ba534c2e030ab3aacb924ee
Alex Kussauer <akussauer@gmail.com>
2026-06-25 13:17:01 +0100

    changes

--- pages/2_Experience.py
+++ pages/2_Experience.py
@@ -11,7 +11,7 @@ st.set_page_config(
 st.header("💼 Work Experience")
 st.markdown("""
             ##### Performance Analyst (ad hoc)
-            AquaticsGB: Jan 2025 - Present
+            **AquaticsGB**: Jan 2025 - Present
 
             - Perform notational analysis of swimming competitions
             - Develop custom programs for performance prediction and data visualization
@@ -20,7 +20,7 @@ st.write("")
 
 st.markdown("""
             ##### Biomechanics and Data Support (ad hoc)
-            GB Cycling: Aug 2025 - Sept 2025
+            **GB Cycling**: Aug 2025 - Sept 2025
 
             - Provided project support for the biomechanics team at British Cycling in Manchester, England
             - Assisted with data collection and statistical analysis for cycling performance
@@ -30,7 +30,7 @@ st.write("")
 
 st.markdown("""
             ##### Skills Coach and Support Worker
-            Canucks Autism Network: Nov 2021 - Sept 2024
+            **Canucks Autism Network**: Nov 2021 - Sept 2024
 
             - Support children and adults with autism in sports and recreation
             - Teach swimming and water safety to children with autism
@@ -40,7 +40,7 @@ st.write("")
 
 st.markdown("""
             ##### Sports Field Maintenance & Machine Operator
-            City of Nanaimo: 2021 - 2024
+            **City of Nanaimo**: 2021 - 2024
 
             - Maintain and repair sports fields and parks in Nanaimo
             - Operate and maintain heavy machinery for field maintenance
@@ -53,7 +53,7 @@ st.write("")
 
 st.markdown("""
             ##### Clinical Assistant & Technician
-            B.D. Mitchell Prosthetics & Orthotics: 2019 - 2021
+            **B.D. Mitchell Prosthetics & Orthotics**: 2019 - 2021
 
             - Assist with the fabrication and fitting of prosthetics and orthotics
             - Maintain and repair prosthetics and orthotics
@@ -63,7 +63,7 @@ st.write("")
 
 st.markdown("""
             ##### Lifeguard & Swim Instructor
-            City of Nanaimo: 2015 - 2021
+            **City of Nanaimo**: 2015 - 2021
 
             - Supervise and ensure the safety of swimmers in the pool
             - Teach swimming lessons to children and adults
@@ -73,7 +73,7 @@ st.write("")
 
 st.markdown("""
             ##### Parks Ambassador
-            City of Nanaimo: April - July 2020
+            **City of Nanaimo**: April - July 2020
 
             - Promote and educate the public about parks and recreation
             - Maintain and clean parks and recreation facilities
@@ -83,7 +83,7 @@ st.write("")
 
 st.markdown("""
             ##### Camp Leader III
-            City of Nanaimo: July - August 2020
+            **City of Nanaimo**: July - August 2020
 
             - Lead and supervise children in summer camp activities
             - Plan and organize camp activities and events
